caac61b8f2a0aebbe88a917ebf8746f3a79f8665
Markus Feng <zzz1112222595@hotmail.com>
2014-10-10 22:14:34 -0400

    Minor fix

---
+++ src/com/markusfeng/SocketRelay/A/SocketHandler.java
@@ -0,0 +1,17 @@
+package com.markusfeng.SocketRelay.A;
+
+import java.io.Closeable;
+
+/**
+ * Used to handle Sockets in conjunction with SocketMachines.
+ * Implementations read objects from a user defined input
+ * and write objects to a user defined input. Reading methods
+ * are defined per-user, while writing methods are defined in the handler.
+ * @author Markus Feng
+ *
+ * @param <T> The type of objects to read and write.
+ */
+public interface SocketHandler<T> extends Runnable, Closeable,
+SocketHandleable<T>, SocketOperator<T>{
+
+}
